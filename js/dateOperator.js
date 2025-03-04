1a7ff35cf95f9f664ab3a99e7b53ba14c0a2ebcf
Shreyansh <jainshreyansh163@gmail.com>
2025-03-04 12:03:45 +0530

    changes to dateOperator.js

--- js/dateOperator.js
+++ js/dateOperator.js
@@ -5,9 +5,9 @@ const parseDateToDays = (date) => {
 };
 
 const formatDate = (date) => {
-    let dd = date.getDate();
-    let mm = date.getMonth() + 1;
-    let yyyy = date.getFullYear();
+    const dd = date.getDate();
+    const mm = date.getMonth() + 1;
+    const yyyy = date.getFullYear();
     if (dd < 10) dd = '0' + dd;
     if (mm < 10) mm = '0' + mm;
     return `${dd}/${mm}/${yyyy}`;
